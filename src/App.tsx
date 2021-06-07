eee74cef62d3f3d0a3c5a045cc896f925c0135a5
Lelik Jan <21alex7777@gmail,com>
2021-06-07 13:11:02 +0300

    simple commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import './App.css';
 import {According} from './stories/According/According';
 import {findAllByDisplayValue} from '@testing-library/react';
 
-export type ItemsType={
-    name:string;
-    value:any;
+export type ItemsType = {
+    name: string;
+    value: any;
 }
+
 function App() {
     const [on, setOn] = useState<boolean>(false);
-const items:ItemsType[]=[
-    {name:'alex',value:1},
-    {name:'bob',value:2},
-    {name:'nick',value:3},
-];
+    const items: ItemsType[] = [
+        {name: 'alex', value: 1},
+        {name: 'bob', value: 2},
+        {name: 'nick', value: 3},
+        {name: 'nick2', value: 4},
+    ];
+
     function changeCollapse(col: boolean) {
         setOn(!col);
     }
